65a787a8d9e8457e6ab205226b2d6291e15d9040
Eshwar <142719089+3aswarconnect@users.noreply.github.com>
2025-03-14 15:03:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const {DynamoDBClient,ListTablesCommand, PutItemCommand}=require("@aws-sdk/client-dynamodb");
+
+const client=new DynamoDBClient({
+    region:'us-east-1',
+    credentials:{
+        accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
+
+async function main(){
+    const listTablesCommand=new ListTablesCommand();
+    const res=await client.send(listTablesCommand);
+    console.log({tables:res.TableNames});
+
+    const putItemCommand=new PutItemCommand({
+        TableName:'youtube-demo',
+        Item:{
+            serial_no:{
+                "S":"0001"
+            },
+            name:{
+                "S":"ajdd"
+            }
+        }
+    });
+    await client.send(putItemCommand);
+
+}
+main();
